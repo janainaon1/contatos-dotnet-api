2ac690768c7747916ad650a6b5dcfdb3fe15f6b9
Janaina Oliveira <janaina@dnaxis.com.br>
2023-04-21 15:16:59 -0300

    feat: Swagger in prod

--- ContatosAPI/Program.cs
+++ ContatosAPI/Program.cs
@@ -47,11 +47,11 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
